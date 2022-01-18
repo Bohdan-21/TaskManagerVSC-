2ad0b69338eb8c7bf907f2365ed7ca195e459d57
Skerskroul <spiderpoisenqwerty@gmail.com>
2022-01-18 11:37:59 +0200

    remake but do this bad

--- TaskManagerVSC++/Special.cpp
+++ TaskManagerVSC++/Special.cpp
@@ -1,27 +1,45 @@
 #include "Special.h"
 
-shared_ptr<stack<string>> copyStack(shared_ptr<stack<string>> copyStack) noexcept//TODO:rename
+shared_ptr<stack<string>> copyStack(shared_ptr<stack<string>> copyStack) noexcept
 {
-	size_t sizeAllTasks;
+	return copy(copyStack);
+}
 
-	sizeAllTasks = copyStack->size();
+shared_ptr<stack<string>> copy(shared_ptr<stack<string>> objectForCopy)
+{
+	size_t sizeCopyObject = getSizeObject(objectForCopy);
+	shared_ptr<string[]> objectForCopyInArray = operationCopy(objectForCopy, sizeCopyObject);
 
-	unique_ptr<string[]> arr(new string[sizeAllTasks]);
-	shared_ptr<stack<string>> result = shared_ptr<stack<string>>(new stack<string>);
+	return operationPaste(objectForCopyInArray, objectForCopy, sizeCopyObject);
+}
 
-	for (size_t i = 0; i < sizeAllTasks; i++)
+shared_ptr<string[]> operationCopy(shared_ptr<stack<string>> objectForCopy, size_t sizeCopyObject)
+{
+	shared_ptr<string[]> arr(new string[sizeCopyObject]);
+
+	for (size_t i = 0; i < sizeCopyObject; i++)
 	{
-		arr[i] = copyStack->top();
-		copyStack->pop();
+		arr[i] = objectForCopy->top();
+		objectForCopy->pop();
 	}
-	
-	for (size_t i = sizeAllTasks - 1; i != -1; i--)
-	{
-		copyStack->push(arr[i]);
-		result->push(arr[i]);
-	}
+	return arr;
+}
 
+shared_ptr<stack<string>> operationPaste(shared_ptr<string[]> objectForPaste, shared_ptr<stack<string>> onjectForCopy, size_t sizeCopyObject)
+{
+	shared_ptr<stack<string>> result(new stack<string>);
+
+	for (size_t i = sizeCopyObject - 1; i != -1; i--)
+	{
+		onjectForCopy->push(objectForPaste[i]);
+		result->push(objectForPaste[i]);
+	}
 	return result;
+}
+
+size_t getSizeObject(shared_ptr<stack<string>> objectToGetTheSize)
+{
+	return objectToGetTheSize->size();
 }
 
 shared_ptr<stack<string>> reverseStack(shared_ptr<stack<string>> stackForReverse)noexcept//TODO:dont use this
